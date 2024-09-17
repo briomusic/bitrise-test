41e63f1a1d7dac191b93e11dc6e02b3ff5630740
Brio <info@briomusic.net>
2024-09-17 15:29:01 +0200

    add project

---
+++ bitrise-test/bitrise-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bitrise-test
+//
+//  Created by Brio Taliaferro on 17.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
